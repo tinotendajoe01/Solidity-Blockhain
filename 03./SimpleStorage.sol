83006532c5ad04673501de87ec2e84aec2816f96
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-06-16 09:33:46 +0200

    memory

--- 03./SimpleStorage.sol
+++ 03./SimpleStorage.sol
@@ -4,13 +4,25 @@ pragma solidity ^0.8.19; //stating our version
 
 contract  SimpleStorage {
  
-    uint256 public  favoriteNumber;//defaulted to 0 i.e uint256 favoriteNumber =0
+    uint256  myFavoriteNumber;//defaulted to 0 i.e uint256 favoriteNumber =0
+//uint256[] listofFavoriteNumbers;//[8,6,4,3]
+ struct Person {
+     uint256 favoriteNumber;
+     string name;
+ }
+// Person public myFriend = Person(7, 'James');
+Person[] public listofPeople; //dynamic array
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
   }
   //view ,pure
    function retrieve( ) public view returns(uint256){
-       return favoriteNumber;
+       return myFavoriteNumber;
+   }
+   function addPerson(string memory _name, uint256 _favoriteNumber) public{
+       
+       
+       listofPeople.push(Person(_favoriteNumber ,_name));
    }
 }
