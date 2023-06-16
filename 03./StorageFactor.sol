a872ac7cbce53a6a83f04b5915451c987eaf6dfb
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-06-16 17:13:16 +0200

    inheritence

--- 03./StorageFactor.sol
+++ 03./StorageFactor.sol
@@ -1,10 +1,33 @@
-//SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.19; 
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// import {SimpleStorage, SimpleStorage2} from "./SimpleStorage.sol";
 import {SimpleStorage} from "./SimpleStorage.sol";
+
 contract StorageFactory {
-    SimpleStorage public simpleStorage;
-    
+    SimpleStorage[] public listOfSimpleStorageContracts;
+
     function createSimpleStorageContract() public {
-        simpleStorage = new SimpleStorage();
+        SimpleStorage simpleStorageContractVariable = new SimpleStorage();
+        // SimpleStorage simpleStorage = new SimpleStorage();
+        listOfSimpleStorageContracts.push(simpleStorageContractVariable);
+    }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // Address
+        // ABI
+        // SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+        listOfSimpleStorageContracts[_simpleStorageIndex].store(
+            _simpleStorageNumber
+        );
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        // return SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).retrieve();
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
     }
 }
