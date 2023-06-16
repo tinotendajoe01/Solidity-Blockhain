084402db566ba3d1b8d41fc42a7f74a821dec324
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-06-16 11:00:23 +0200

    other fx

--- 03./SimpleStorage.sol
+++ 03./SimpleStorage.sol
@@ -28,4 +28,40 @@ mapping (string => uint256) public nameToFavoriteNumber;
        listofPeople.push(Person(_favoriteNumber ,_name));
        nameToFavoriteNumber[_name]= _favoriteNumber;
    }
+   function getPersonFavoriteNumber(string calldata  _name) public view returns (uint256) {
+       return nameToFavoriteNumber[_name] ; 
+   }
+function getTotalPeople() public view returns (uint256) {
+    return listofPeople.length;
+}
+
+function getPersonAtIndex (uint256 i) public view returns(string memory, uint256){
+    require(i< listofPeople.length, "indext out of bounds");
+    Person memory p = listofPeople[i];
+
+return (p.name, p.favoriteNumber);
+}
+function deletePerson(string memory _name) public {
+    require(listofPeople.length > 0, "No people in the list");
+    
+    uint256 indexToDelete = 0;
+    bool found = false;
+    
+    for (uint256 i = 0; i < listofPeople.length; i++) {
+        if (keccak256(bytes(listofPeople[i].name)) == keccak256(bytes(_name))) {
+            indexToDelete = i;
+            found = true;
+            break;
+        }
+    }
+    
+    require(found, "Person not found");
+    
+    for (uint256 i = indexToDelete; i < listofPeople.length - 1; i++) {
+        listofPeople[i] = listofPeople[i+1];
+    }
+    
+    listofPeople.pop();
+    delete nameToFavoriteNumber[_name];
+}
 }
