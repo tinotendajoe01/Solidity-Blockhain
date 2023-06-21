f241e2f426c32685ca8add5781378e0413982929
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-06-21 09:13:03 +0200

    chore: forge init

---
+++ 06.FoundryFundMe/test/FundMeTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+import { Test } from "forge-std/Test.sol";
+
+contract FundmeTest is Test {
+	function setUp() external {}
+
+	function testDemo() public {}
+}
