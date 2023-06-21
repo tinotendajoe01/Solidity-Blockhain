23d4b2ec7361cbd47d63d5ff483ea98e1c4601df
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-06-21 10:03:26 +0200

    tests

--- 06.FoundryFundMe/src/Fundme.sol
+++ 06.FoundryFundMe/src/Fundme.sol
@@ -1,62 +1,160 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import {PriceConverter} from "./PriceConverter.sol";
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import { PriceConverter } from "./PriceConverter.sol";
 
-error FundMe__NotOwner();
+//a custom error
+error FundMe_NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
-
+    //a mapping to keep tract of how much each funder has funded
     mapping(address => uint256) public addressToAmountFunded;
+    //an array to keep track of all funders
     address[] public funders;
+    address payable public fundsDestination;
+    // immutable contract owner
+    address public i_owner; /* immutable */
+    uint256 public constant MINIMUM_USD = 5e18; //minimu ammount that can be funded in usd  5 * 10 ** 18;
+    //goal
+    uint256 public maxGoal;
+    uint256 public deadline;
 
-    // Could we make this constant?  /* hint: no! We should make it immutable! */
-    address public /* immutable */ i_owner;
-    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
-    
+    // an event to be emitted when new funds are donated
+    event NewDonation(address indexed funder, uint256 amount);
+
+    // this constructor sets the contract owner to the deployer
     constructor() {
         i_owner = msg.sender;
     }
 
-    function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
-        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
-        addressToAmountFunded[msg.sender] += msg.value;
-        funders.push(msg.sender);
+    // a struct to store the details of a funder
+    struct Funder {
+        address funderAddress;
+        uint256 amountFunded;
     }
-    
-    function getVersion() public view returns (uint256){
+
+    function setMaxGoal(uint256 _maxGoal) public onlyOwner {
+        maxGoal = _maxGoal;
+    }
+
+    function setDeadline(uint256 _deadline) public onlyOwner {
+        deadline = _deadline;
+    }
+
+    function fund() public payable {
+        // Check that the amount of ETH being sent is at least the minimum amount in USD
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "You need to spend more ETH!");
+
+        // Check that the maximum goal has not been reached yet
+        if (maxGoal > 0) {
+            require(address(this).balance + msg.value <= maxGoal, "Maximum goal reached!");
+        }
+
+        // Check if the deadline has passed
+        if (deadline > 0) {
+            require(block.timestamp <= deadline, "Deadline has passed, cannot fund!");
+        }
+
+        // Update the mapping with the amount funded by the sender
+        addressToAmountFunded[msg.sender] += msg.value;
+
+        // Add the sender to the list of funders
+        funders.push(msg.sender);
+
+        if (maxGoal > 0 && address(this).balance == maxGoal) {
+            // Transfer the funds to the destination address
+            fundsDestination.transfer(address(this).balance);
+        }
+        emit NewDonation(msg.sender, msg.value);
+    }
+
+    //get the version of chainlink pricefeed being use--- help in geting the contract ABI
+    function getVersion() public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         return priceFeed.version();
     }
-    
-    modifier onlyOwner {
-        // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert FundMe__NotOwner();
+
+    // a function to check if the deadline has passed
+    function hasDeadlinePassed() public view returns (bool) {
+        return (deadline > 0 && block.timestamp > deadline);
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner, "sender not owner");
+        if (msg.sender != i_owner) revert FundMe_NotOwner();
         _;
     }
-    
+
     function withdraw() public onlyOwner {
-        for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
         // // transfer
         // payable(msg.sender).transfer(address(this).balance);
-        
+
         // // send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
 
         // call
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{ value: address(this).balance }("");
         require(callSuccess, "Call failed");
     }
 
+    function setFundsDestination(address payable _fundsDestination) public onlyOwner {
+        fundsDestination = _fundsDestination;
+    }
 
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdrawFunds() public {
+        if (maxGoal > 0) {
+            require(address(this).balance < maxGoal, "Max balance reached, cannot withdraw funds");
+        }
+        uint256 ammountFunded = addressToAmountFunded[msg.sender];
+        require(ammountFunded > 0, "Sender has not funded any amount");
+        addressToAmountFunded[msg.sender] = 0;
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            if (funders[funderIndex] == msg.sender) {
+                funders[funderIndex] = funders[funders.length - 1];
+                funders.pop();
+                break;
+            }
+        }
+        payable(msg.sender).transfer(ammountFunded);
+    }
+
+    function getDonations() public view returns (Funder[] memory) {
+        Funder[] memory donations = new Funder[](funders.length);
+
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            uint256 amount = addressToAmountFunded[funder];
+            donations[i] = Funder(funder, amount);
+        }
+
+        return donations;
+    }
+
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    //call when the contract receive ETH without any data
     fallback() external payable {
         fund();
     }
@@ -64,8 +162,4 @@ contract FundMe {
     receive() external payable {
         fund();
     }
-
 }
-
-
-
--- 06.FoundryFundMe/test/FundMeTest.t.sol
+++ 06.FoundryFundMe/test/FundMeTest.t.sol
@@ -1,16 +1,24 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
 import { Test, console } from "forge-std/Test.sol";
+import { FundMe } from "../src/Fundme.sol";
 
 contract FundmeTest is Test {
-    uint256 number = 3;
+    FundMe fundMe;
 
     function setUp() external {
-        number = 2;
+        fundMe = new FundMe();
     }
 
-    function testDemo() public {
-        console.log(number);
-        assertEq(number, 2);
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMesgSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+
+    function testHasDeadlinePassed() public {
+        assertEq(fundMe.hasDeadlinePassed(), false, "Deadline check result should match expected result");
     }
 }
