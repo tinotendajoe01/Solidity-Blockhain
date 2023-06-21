c052af7a8397e717887979e139818aee2c452207
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-06-21 09:36:39 +0200

    ok

--- 06.FoundryFundMe/src/Fundme.sol
+++ 06.FoundryFundMe/src/Fundme.sol
@@ -34,7 +34,7 @@ contract FundMe {
     
     modifier onlyOwner {
         // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
     
--- 06.FoundryFundMe/test/FundMeTest.t.sol
+++ 06.FoundryFundMe/test/FundMeTest.t.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.20;
-import { Test } from "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
 
 contract FundmeTest is Test {
-	function setUp() external {}
+    uint256 number = 3;
 
-	function testDemo() public {}
+    function setUp() external {
+        number = 2;
+    }
+
+    function testDemo() public {
+        console.log(number);
+        assertEq(number, 2);
+    }
 }
