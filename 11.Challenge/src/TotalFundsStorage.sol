c123e4e3ef7afe3641198c553d1cfa615cb717de
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-07-02 18:41:55 +0200

    storage

--- 11.Challenge/src/TotalFundsStorage.sol
+++ 11.Challenge/src/TotalFundsStorage.sol
@@ -2,21 +2,12 @@
 pragma solidity 0.8.20;
 
 contract TotalFundsStorage {
-	uint256 totalProjectFunds;
 	struct Project {
 		string name;
 		uint256 totalFundsInProject;
 	}
 	Project[] public listofProjects;
 	mapping(string => uint256) public projectNameToTotalFunds;
-
-	function store(uint256 _totalProjectFunds) public virtual {
-		totalProjectFunds = _totalProjectFunds;
-	}
-
-	function retriev() public view returns (uint256) {
-		return totalProjectFunds;
-	}
 
 	function addProject(
 		string memory _name,
