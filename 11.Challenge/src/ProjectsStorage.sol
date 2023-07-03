1607a57b954a38c5274f58edb02e30b0c1887ae0
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-07-03 12:55:57 +0200

    projectsStorage Tests

--- 11.Challenge/src/ProjectsStorage.sol
+++ 11.Challenge/src/ProjectsStorage.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.20;
 error ProjectsStorage__NotOwner();
+error Project_Does_Not_Exist();
+error Funding_Goal_CanntoBeLess_Than_Zero();
+error Name_Exists();
+error ProjectNam_CntBEmpty();
 
 contract ProjectsStorage {
 	uint256 private constant INITIAL_BALANCE = 0;
-	uint256 private projectIdCounter;
+	uint256 public projectIdCounter;
 	address private immutable i_owner;
 
 	constructor() {
@@ -30,6 +34,10 @@ contract ProjectsStorage {
 		string memory _name,
 		uint256 _fundingGoal
 	) public onlyOwner {
+		if (_fundingGoal <= 0) revert Funding_Goal_CanntoBeLess_Than_Zero();
+		if (projectNameAlreadyExists(_name) == true) revert Name_Exists();
+		if (keccak256(bytes(_name)) == keccak256(bytes("")))
+			revert ProjectNam_CntBEmpty();
 		projectIdCounter++;
 		listofProjects.push(
 			Project(projectIdCounter, _name, _fundingGoal, 0, new address[](0))
@@ -37,14 +45,33 @@ contract ProjectsStorage {
 		projectNameToId[_name] = projectIdCounter;
 	}
 
+	function projectNameAlreadyExists(
+		string memory projectName
+	) public view returns (bool) {
+		for (uint256 i = 0; i < listofProjects.length; i++) {
+			if (
+				keccak256(bytes(listofProjects[i].name)) ==
+				keccak256(bytes(projectName))
+			) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	// Getter function to retrieve the funders for a project
 	function getProjectFunders(
 		string calldata _name
 	) public view returns (address[] memory) {
 		uint256 projectId = projectNameToId[_name];
-		require(projectId != 0, "Project does not exist");
+		// require(projectId != 0, "Project does not exist");
+		if (projectId != 0) revert Project_Does_Not_Exist();
 
 		Project storage project = listofProjects[projectId - 1];
 		return project.funders;
+	}
+
+	function getProjectsLength() public view returns (uint256) {
+		return listofProjects.length;
 	}
 }
--- 11.Challenge/test/testProjectsStorage.t.sol
+++ 11.Challenge/test/testProjectsStorage.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.20;
-import { Test } from "forge-std/Test.sol";
+import { Test, console } from "forge-std/Test.sol";
+
 import { ProjectsStorage } from "../src/ProjectsStorage.sol";
 import { DeployProjectsStorage } from "../script/DeployProjectsStorage.s.sol";
 
@@ -10,5 +11,53 @@ contract TestProjectsStorage is Test {
 	function setUp() external {
 		DeployProjectsStorage deployer = new DeployProjectsStorage();
 		projectsStorage = deployer.run();
+	}
+
+	function testOnlyOwnerCanAddProject() public {
+		vm.expectRevert(); //must fail not owner
+		// vm.prank(msg.sender); //user owner addres
+		projectsStorage.addProject("Football", 400);
+	}
+
+	function testProjectIsCreatedWithUniqueidAndAddedToListOfProjects() public {
+		vm.startPrank(msg.sender);
+		projectsStorage.addProject("Football", 400);
+		projectsStorage.addProject("Basketball", 500);
+		projectsStorage.addProject("Tennis", 600);
+		uint256 expectedId = 3;
+		uint256 actualId = projectsStorage.projectIdCounter();
+		uint256 expectedLenght = 3;
+		uint256 actualLength = projectsStorage.getProjectsLength();
+
+		assertEq(expectedId, actualId);
+		assertEq(expectedLenght, actualLength);
+	}
+
+	function testProjectNameMappedToId() public {
+		vm.startPrank(msg.sender);
+		projectsStorage.addProject("Football", 400);
+		projectsStorage.addProject("Basketball", 500);
+		projectsStorage.addProject("Tennis", 600);
+		uint256 basketballId = 2;
+		uint256 actualBasketBallId = projectsStorage.projectNameToId(
+			"Basketball"
+		);
+		assertEq(basketballId, actualBasketBallId);
+	}
+
+	function testProjectCannotBeCreatedWithZeroFundingBalance() public {
+		vm.expectRevert(); //must fail no goal
+
+		projectsStorage.addProject("Football", 0);
+	}
+
+	function testProjectIsNotCreatedWithDupicateName() public {
+		vm.startPrank(msg.sender);
+		projectsStorage.addProject("Football", 400);
+		vm.expectRevert();
+		projectsStorage.addProject("Football", 500);
+
+		vm.expectRevert();
+		projectsStorage.addProject("", 500);
 	}
 }
