b1d94d1979f43d89d9ccf52753c733491ceca4d5
tinotendajoe01 <tinotendajoe01@gmail.com>
2023-07-17 17:34:21 +0200

    DSC Deployer

--- 12.DeFi-StableCoin/script/DeployDSC.s.sol
+++ 12.DeFi-StableCoin/script/DeployDSC.s.sol
@@ -6,27 +6,63 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
 
+/**
+ * @title DeployDSC
+ * @author Tinotenda Joe
+ * @dev Deploys the DecentralizedStableCoin, the DSCEngine and setup HelperConfig.
+ * @return An instance of the DecentralizedStableCoin, DSCEngine and HelperConfig.
+ */
 contract DeployDSC is Script {
     address[] public tokenAddresses;
     address[] public priceFeedAddresses;
 
+    /**
+     * @dev This function is the main runner for the Deployer.
+     * It deploys instances of HelperConfig, DecentralizedStableCoin, and DSCEngine,
+     * sets up the token addresses and price feed addresses,
+     * and starts and stops a broadcast for deploying these contracts.
+     * @return An instance of the DecentralizedStableCoin, DSCEngine, and HelperConfig.
+     */
+
     function run() external returns (DecentralizedStableCoin, DSCEngine, HelperConfig) {
-        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        // Deploy a new HelperConfig to inject mocks.
+
+        HelperConfig helperConfig = new HelperConfig();
+
+        // Get active network configurations from helperConfig.
+        // This includes the price feed addresses for weth and wbtc,
+        // the token addresses for weth and wbtc, and deployer key.
 
         (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
             helperConfig.activeNetworkConfig();
+
+        // Set token addresses, including weth and wbtc.
         tokenAddresses = [weth, wbtc];
+
+        // Set price feed addresses for weth and wbtc.
         priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
 
+        // Start a broadcast with a deployer key.
+        // This will broadcast the deployment of the following contracts.
         vm.startBroadcast(deployerKey);
+
+        // Deploy a new DecentralizedStableCoin contract.
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
+
+        // Deploy a new DSCEngine contract and pass token addresses,
+        // price feed addresses, and the address of DecentralizedStableCoin to it.
         DSCEngine dscEngine = new DSCEngine(
     tokenAddresses,
     priceFeedAddresses,
     address(dsc)
     );
+
+        // Transfer the ownership of the DecentralizedStableCoin to the DSCEngine.
         dsc.transferOwnership(address(dscEngine));
+        // Stop the broadcast after deploying the contracts.
         vm.stopBroadcast();
+
+        // Return the instances of DecentralizedStableCoin, DSCEngine, and HelperConfig.
         return (dsc, dscEngine, helperConfig);
     }
 }
--- 12.DeFi-StableCoin/script/HelperConfig.s.sol
+++ 12.DeFi-StableCoin/script/HelperConfig.s.sol
@@ -5,13 +5,17 @@ import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 import {Script} from "forge-std/Script.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
+/**
+ * @title HelperConfig
+ * @dev The contract used for configuring different environments
+ */
 contract HelperConfig is Script {
-    NetworkConfig public activeNetworkConfig;
-
+    // Define constants for prices and decimal precision
     uint8 public constant DECIMALS = 8;
     int256 public constant ETH_USD_PRICE = 2000e8;
     int256 public constant BTC_USD_PRICE = 1000e8;
 
+    // Define a struct to hold different network configuration parameters
     struct NetworkConfig {
         address wethUsdPriceFeed;
         address wbtcUsdPriceFeed;
@@ -20,8 +24,11 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
+    // Instance of NetworkConfig to hold the active network configuration
+    NetworkConfig public activeNetworkConfig;
     uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
+    // Constructor that sets the active network configuration based on the chain id
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
@@ -30,7 +37,9 @@ contract HelperConfig is Script {
         }
     }
 
+    // Method to get the Sepolia network configuration
     function getSepoliaEthConfig() public view returns (NetworkConfig memory sepoliaNetworkConfig) {
+        // Returns predefined Sepolia network configuration
         sepoliaNetworkConfig = NetworkConfig({
             wethUsdPriceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306, // ETH / USD
             wbtcUsdPriceFeed: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
@@ -40,26 +49,34 @@ contract HelperConfig is Script {
         });
     }
 
+    // Method to create or return the Anvil network configuration
     function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory anvilNetworkConfig) {
-        // Check to see if we set an active network config
+        // Check if already set the active network config
         if (activeNetworkConfig.wethUsdPriceFeed != address(0)) {
             return activeNetworkConfig;
         }
 
+        // Start the broadcast
         vm.startBroadcast();
+
+        // Deploy mock V3Aggregator for ETH/USD price feed and a mock WETH token
         MockV3Aggregator ethUsdPriceFeed = new MockV3Aggregator(
-    DECIMALS,
-    ETH_USD_PRICE
-    );
+            DECIMALS,
+            ETH_USD_PRICE
+        );
         ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
 
+        // Deploy mock V3Aggregator for BTC/USD price feed and a mock WBTC token
         MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(
-    DECIMALS,
-    BTC_USD_PRICE
-    );
+            DECIMALS,
+            BTC_USD_PRICE
+        );
         ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
+
+        // Stop the broadcast
         vm.stopBroadcast();
 
+        // Returns the Anvil network configuration with mock contracts and predefined deployer key
         anvilNetworkConfig = NetworkConfig({
             wethUsdPriceFeed: address(ethUsdPriceFeed), // ETH / USD
             weth: address(wethMock),
